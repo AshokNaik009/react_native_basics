5ba488bc5c2daf42eae50e6c2d9b69606b279187
tag8ashok <naikashok08@gmail.com>
2019-10-11 21:34:43 +0530

    Passing   callback to child

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,10 +3,15 @@ import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import ColorCounter from './components/ColorCounter'
 
 const SquareScreen = () => {
+
+    const [red,setRed] = useState(0);
+    const [green,setGreen] = useState(0);
+    const [blue,setBlue] = useState(0);
+
     return <View>
-        <ColorCounter color="Red" />
-        <ColorCounter color="Green" />
-        <ColorCounter color="Blue" />
+        <ColorCounter increaseColor={ () => setRed(red+1) } decreaseColor={ () => setRed(red-1) } color="Red" />
+        <ColorCounter increaseColor={ () => setRed(green+1) } decreaseColor={ () => setRed(green-1) } color="Green" />
+        <ColorCounter increaseColor={ () => setRed(blue+1) } decreaseColor={ () => setRed(blue-1) }  color="Blue" />
     </View>
 }
 
--- src/screens/components/ColorCounter.js
+++ src/screens/components/ColorCounter.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-const ColorCounter = ({color}) => {
+const ColorCounter = ({color , increaseColor , decreaseColor}) => {
     return (<View>
         <Text>{color}</Text>
-        <Button title={`Increase ${color}`} ></Button>
-        <Button title={`Increase ${color}`} ></Button>
+        <Button onPress={ increaseColor() } title={`Increase ${color}`} ></Button>
+        <Button onPress={ decreaseColor() } title={`Increase ${color}`} ></Button>
 
     </View>);
 }
