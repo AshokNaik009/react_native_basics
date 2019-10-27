6d883f462a1af00083d187a500d1174f2001a2da
tag8ashok <naikashok08@gmail.com>
2019-10-27 13:13:10 +0530

    Added the implemetation of Reducer in SquareScreen

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,43 +1,72 @@
-import React, { useState } from 'react';
+import React, { useReducer} from 'react';
 import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import ColorCounter from './components/ColorCounter'
 
 const COLORINCREMENT = 15;
 
+
+const reducer = (state,action) => {
+
+        //state === { red:number , green: number , blue:number}
+        //action === { colorToChange , amount}
+
+        switch(action.colortoChange) {
+
+            case 'red' :
+                return state.red + action.amount > 255 || state.red + action.amount < 0 ? state : {...state,red: state.red + action.amount}                    
+            case 'green' :
+                return state.green + action.amount > 255 || state.green + action.amount < 0 ? state : {...state,green: state.green + action.amount}
+            case 'blue' :
+                return state.blue + action.amount > 255 || state.blue + action.amount < 0 ? state : {...state,blue: state.blue + action.amount}
+
+            default:
+                return state;
+
+        }
+
+}
+
 const SquareScreen = () => {
 
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+
+    const [state,dispatch] = useReducer(reducer, {red:0,green:0,blue:0});
+  //  console.log(state); // {red:0,green:0,blue:0}
+    const {red , green , blue } = state;
+    //dispactch method is also called as runMyReducer
+
+
+    // const [red, setRed] = useState(0);
+    // const [green, setGreen] = useState(0);
+    // const [blue, setBlue] = useState(0);
     
 
     //Creating a helper Function
-    const setColor = (color,change) => {
-        //Color Paramter wil be the color we want to Change
-        //Change will be +15 or -15
+    // const setColor = (color,change) => {
+    //     //Color Paramter wil be the color we want to Change
+    //     //Change will be +15 or -15
 
-        switch(color) {
-         case 'red':
-             red + change > 255 || red + change < 0 ? null : setRed(red+change)
-             return
-         case 'green':
-             green + change > 255 || green + change < 0 ? null : setGreen(green+change)
-             return
-         case 'blue':
-             blue + change > 255 || blue + change < 0 ? null : setBlue(blue+change)
-             return
+    //     switch(color) {
+    //      case 'red':
+    //          red + change > 255 || red + change < 0 ? null : setRed(red+change)
+    //          return
+    //      case 'green':
+    //          green + change > 255 || green + change < 0 ? null : setGreen(green+change)
+    //          return
+    //      case 'blue':
+    //          blue + change > 255 || blue + change < 0 ? null : setBlue(blue+change)
+    //          return
 
-         default:
-             return
+    //      default:
+    //          return
          
-        }
-    }
+    //     }
+    // }
 
     return (
         <View>
-            <ColorCounter increaseColor={() => setColor('red' , COLORINCREMENT)} decreaseColor={() => setColor('red'  , -1 * COLORINCREMENT)} color="Red" />
-            <ColorCounter increaseColor={() => setColor('green' , COLORINCREMENT)} decreaseColor={() => setColor('green' ,  -1 * COLORINCREMENT)} color="Green" />
-            <ColorCounter increaseColor={() => setColor('blue' , COLORINCREMENT)} decreaseColor={() => setColor('blue' , -1 * COLORINCREMENT)} color="Blue" />
+            <ColorCounter increaseColor={() => dispatch({colortoChange: 'red',amount:COLORINCREMENT}) } decreaseColor={() => dispatch({colortoChange: 'red',amount: -1 * COLORINCREMENT}) } color="Red" />
+            <ColorCounter increaseColor={() => dispatch({colortoChange: 'green',amount:COLORINCREMENT}) } decreaseColor={() => dispatch({colortoChange: 'green',amount:-1 * COLORINCREMENT}) } color="Green" />
+            <ColorCounter increaseColor={() => dispatch({colortoChange: 'blue',amount:COLORINCREMENT}) } decreaseColor={() => dispatch({colortoChange: 'blue',amount:-1 * COLORINCREMENT}) } color="Blue" />
 
             <View
                 style={{
